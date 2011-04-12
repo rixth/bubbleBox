512d3543d1cf0374fa9697197f50a8afdd3badd7
Tom Rix <tom@rixth.org>
2011-04-12 16:07:27 -0700

    Base cut working with some specs fleshed out

--- src/jquery.bubbleBox.js
+++ src/jquery.bubbleBox.js
@@ -1,5 +1,69 @@
 (function ($) {
   $.widget("ui.bubbleBox", {
-
+    options: {
+      showRemoveButton: true,
+      list: null,
+      allowDupes: false,
+      seedData: [],
+      triggerKeyCodes: [
+        13,      // enter
+        44, 188, // the two possibilities for comma
+        59, 186  // the two possibilities for semicolon
+      ]
+    },
+    _create: function () {
+      var self = this;
+      
+      self.input = self.element;
+      
+      self.options.list || (self.options.list = '#' + self.input.attr('id') + '_list');
+      self.list = $(self.options.list).addClass('ui-bubblebox-list');
+      
+      // Bind events
+      if (self.options.showRemoveButton) {
+        self.list.delegate('.ui-bubbleBox-removeItem', 'click', function (event) {
+          $(this.parentNode).remove();
+          self._trigger('remove', event, {value: $(this).text, node: this});
+        });
+      }
+      
+      self.element.bind('blur keypress', function (event) {
+        if (event.type === 'blur' || self.options.triggerKeyCodes.indexOf(event.which) !== -1) {
+          var bubbleValue = self.input.val();
+          
+          if (bubbleValue !== '' && !bubbleValue.match(/^\s+$/) && self._trigger('beforeAdd', event, { value: bubbleValue }) !== false) {
+            self.input.val('');
+            self._trigger('afterAdd', event, { value: bubbleValue, node: self.addItem(bubbleValue) });
+          }
+          
+          event.preventDefault();
+        }
+      });
+      
+      // Insert start data
+      $(self.options.seedData).each(function () {
+        self.addItem(this);
+      });
+    },
+    _createBubble: function (value) {
+      var removeButton = this.options.showRemoveButton ? '<div class="ui-bubbleBox-removeItem">x</div>' : '';
+      return $('<li class="ui-bubblebox-item"><span>' + value + '</span>' + removeButton + '</li>');
+    },
+    val: function () {
+      var values = [];
+      this.list.children().each(function () {
+        values.push($(this).find('span').text())
+      })
+      return values.join(',');
+    },
+    addItem: function (value) {
+      var bubble = this._createBubble(value);
+      this.list.append(bubble);
+      return bubble;
+    },
+    _destroy: function () {
+      this.list.empty().removeClass('ui-bubblebox-list');
+      $.Widget.prototype.destroy.apply(this, arguments);
+    }
   });
 }(jQuery));
--- test/bubbleBox.spec.js
+++ test/bubbleBox.spec.js
@@ -1,86 +1,113 @@
-beforeEach(function() {
-  setFixtures('<ul id="bubbleBox_list"></ul><div id="bubbleBox"></div>');
-  $('#bubbleBox').bubbleBox();
-  expect($('#bubbleBox').length).toEqual(1);
-});
+describe("bubblebox", function () {
+  function resetHtml() {
+    setFixtures('<ul id="bubbleBox_list"></ul><div id="bubbleBox"></div>');
+  }
 
-describe("adding items", function () {
-  var enterEvent = $.Event('keypress'),
-      commaEvent = $.Event('keypress'),
-      semicolonEvent1 = $.Event('keypress'),
-      semicolonEvent2 = $.Event('keypress');
-  
-  enterEvent.which = $.ui.keyCode.ENTER;
-  commaEvent.which = $.ui.keyCode.COMMA;
-  semicolonEvent1.which = 59; // firefox's code
-  semicolonEvent2.which = 186; // the standard code
-  
-  it("should do nothing when the input is empty", function () {
-    
+  var itemSelector = '.ui-bubblebox-item';
+
+  beforeEach(function() {
+    resetHtml();
+    $('#bubbleBox').bubbleBox();
+    expect($('#bubbleBox').length).toEqual(1);
   });
-  it("should add a new item when enter is pressed", function () {
-    expect(true).toBeTruthy();
+
+  describe("adding items", function () {
+    var enterEvent = $.Event('keypress');
+    enterEvent.which = $.ui.keyCode.ENTER;
+
+    it("should do nothing when the input is empty", function () {
+      $('#bubbleBox').trigger(enterEvent);
+      expect($('#bubbleBox_list ' + itemSelector).length).toEqual(0);
+    });
+    it("should add a new item when enter is pressed", function () {
+      var box = $('#bubbleBox').val('my value');
+      box.trigger(enterEvent);
+      expect($('#bubbleBox_list ' + itemSelector).length).toEqual(1);
+    });
+    it("should add a new item when comma is pressed", function () {
+      var box = $('#bubbleBox').val('my value'),
+          commaEvent = $.Event('keypress');
+      
+      commaEvent.which = $.ui.keyCode.COMMA;
+      box.trigger(commaEvent);
+      expect($('#bubbleBox_list ' + itemSelector).length).toEqual(1);
+    });
+    it("should add a new item when semicolon is pressed", function () {
+      var box = $('#bubbleBox').val('my value'),
+          semicolonEvent = $.Event('keypress');
+      
+      semicolonEvent.which = 59;
+      box.trigger(semicolonEvent);
+      expect($('#bubbleBox_list ' + itemSelector).length).toEqual(1);
+    });
+    it("should add a new item when input focus is lost", function () {
+      var box = $('#bubbleBox').val('my value');
+      box.trigger($.Event('blur'));
+      expect($('#bubbleBox_list ' + itemSelector).length).toEqual(1);
+    });
+
+    describe("duplicate item handling", function () {
+      it("should not allow duplicates by default", function () {
+        expect(true).toBeTruthy();
+      });
+      it("should allow duplicates when allowDupes is true", function () {
+        expect(true).toBeTruthy();  
+      });
+    });
+
+    describe("seeding start data", function () {
+      it("should add items upon instantiation", function () {
+        resetHtml();
+        $('#bubbleBox').bubbleBox({
+          seedData: ['Trulia', 'Zillow'],
+        });
+        expect($('#bubbleBox_list ' + itemSelector).length).toEqual(2);
+        expect($('#bubbleBox').bubbleBox("val")).toEqual("Trulia,Zillow");
+      });
+    });
   });
-  it("should add a new item when comma is pressed", function () {
-    expect(true).toBeTruthy();
-  });
-  it("should add a new item when semicolon is pressed", function () {
-    expect(true).toBeTruthy();
-  });
-  it("should add a new item when input focus is lost", function () {
-    expect(true).toBeTruthy();
-  });
-  
-  describe("duplicate item handling", function () {
-    it("should not allow duplicates by default", function () {
+
+  describe("removing items", function () {
+    it("should remove items when the close button is clicked", function () {
       expect(true).toBeTruthy();
     });
-    it("should allow duplicates when allowDupes is true", function () {
-      expect(true).toBeTruthy();  
-    });
-  });
-});
-
-describe("removing items", function () {
-  it("should remove items when the close button is clicked", function () {
-    expect(true).toBeTruthy();
-  });
-  it("should remove the last item when the delete key is pressed when the input is blank", function () {
-    expect(true).toBeTruthy();
-  });
-});
-
-describe("events", function () {
-  describe("add events", function () {
-    it("should trigger the beforeAdd event before a new item is added", function () {
-      expect(true).toBeTruthy();
-    });
-    it("should not add an item if any of the beforeAdd handlers return false", function () {
-      expect(1).toBeTruthy();
-    });
-    it("should trigger the afterAdd event after a new item is added", function () {
+    it("should remove the last item when the delete key is pressed when the input is blank", function () {
       expect(true).toBeTruthy();
     });
   });
-  
-  describe("remove events", function () {
-    it("should trigger the remove event when an event is removed", function () {
+
+  describe("events", function () {
+    describe("add events", function () {
+      it("should trigger the beforeAdd event before a new item is added", function () {
+        expect(true).toBeTruthy();
+      });
+      it("should not add an item if any of the beforeAdd handlers return false", function () {
+        expect(1).toBeTruthy();
+      });
+      it("should trigger the afterAdd event after a new item is added", function () {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe("remove events", function () {
+      it("should trigger the remove event when an event is removed", function () {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+
+  describe("fetching the value", function () {
+    it("should put all items comma separated in to another input", function () {
+      expect(true).toBeTruthy();
+    });
+    it("should return comma separated values when the val() method is called", function () {
       expect(true).toBeTruthy();
     });
   });
-});
 
-describe("fetching the value", function () {
-  it("should put all items comma separated in to another input", function () {
-    expect(true).toBeTruthy();
-  });
-  it("should return comma separated values when the val() method is called", function () {
-    expect(true).toBeTruthy();
-  });
-});
-
-describe("destroying", function () {
-  it("should empty the container upon destruction", function () {
-    expect(true).toBeTruthy();
+  describe("destroying", function () {
+    it("should empty the container upon destruction", function () {
+      expect(true).toBeTruthy();
+    });
   });
 });
